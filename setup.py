d696b250d37cf4067518d809ddf642138a8d3290
Leyko, Dmitry <d.leyko@itransition.com>
2020-10-06 19:35:02 +0300

    Update Project

--- setup.py
+++ setup.py
@@ -3,11 +3,9 @@ import setuptools
 with open('README.md', 'r') as f:
     long_description = f.read()
 
-requirements = ['requests<=2.24.0']
-
 setuptools.setup(
     name='hello_world_crock-dev',
-    version='0.0.3',
+    version='1.0.0',
     author='Crock-dev',
     author_email='leiko.dima2013@yandex.by',
     description='A Hello World package',
@@ -15,7 +13,9 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/LeikoDmitry/hello-world',
     packages=setuptools.find_packages(),
-    install_requires=requirements,
+    install_requires=[
+        'requests <= 2.24.0'
+    ],
     classifiers=[
         'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
